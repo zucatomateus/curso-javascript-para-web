69351acfc7c3ad920d6a177154e251ac710f97f8
Mateus Zucato <mateuszucato@hotmail.com>
2022-07-22 23:33:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
- function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+ function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 };
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
-ListaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+//enquanto
+while (contador < ListaDeTeclas.length) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    console.log(instrumento);
+
+    //string template
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick =  function () {
+        tocaSom(idAudio);
+    };
+
+    contador = (contador + 1);
+
+    console.log(contador);
+    console.log(idAudio);
+}
